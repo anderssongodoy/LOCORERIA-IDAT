b659ce232818a131f8d9f605bef5064f07e9cc19
Andersson <anderssongodoygarcia@gmail.com>
2022-04-27 22:41:06 -0500

    Se edito el nombre del archivo principal.py a ventanaLogin.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from Vista.principal import VentanaLogin
+from Vista.ventanaLogin import VentanaLogin
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
